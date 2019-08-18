c4d41ec1a94a8fb1d8f3f03f4ebdc01bb9e9093b
Liam Liden <l14m@msn.com>
2019-08-18 01:12:27 -0500

    add cards to projects page

---
+++ navbar.js
@@ -0,0 +1,15 @@
+document.write('<nav class="navbar navbar-expand-lg navbar-dark bg-primary">\
+    <a class="navbar-brand" href="index.html">PLACEHOLDER</a>\
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">\
+        <span class="navbar-toggler-icon"></span>\
+    </button>\
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">\
+        <div class="navbar-nav">\
+            <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>\
+            <a class="nav-item nav-link" href="resume.html">Resume</a>\
+            <a class="nav-item nav-link" href="projects.html">Projects</a>\
+            <a class="nav-item nav-link" href="about.html">About Me</a>\
+        </div>\
+    </div>\
+</nav>'
+);
